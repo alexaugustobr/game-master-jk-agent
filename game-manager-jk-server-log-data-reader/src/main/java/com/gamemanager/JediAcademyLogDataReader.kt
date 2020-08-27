923597142fbd86c838f5046cfd7c59b6283779ca
alexaugustobr <alexcologia33@gmail.com>
2020-08-26 21:19:28 -0300

    melhorado lógica da leitura de arquivo

--- game-manager-jk-server-log-data-reader/src/main/java/com/gamemanager/JediAcademyLogDataReader.kt
+++ game-manager-jk-server-log-data-reader/src/main/java/com/gamemanager/JediAcademyLogDataReader.kt
@@ -7,6 +7,7 @@ import java.io.IOException
 import java.nio.file.FileSystems
 import java.nio.file.Paths
 import java.nio.file.StandardWatchEventKinds.*
+import java.util.*
 import kotlin.streams.toList
 
 
@@ -18,46 +19,33 @@ class JediAcademyLogDataReader(val file: File) {
 
 	class Chat(val file: File) {
 
-		suspend fun watch(reader: (ChatMessage) -> Unit) = coroutineScope {
+		suspend fun watchChanges(onChangeCallBack: (Message) -> Unit) = coroutineScope {
 			async {
 				try {
-					// Creates a instance of WatchService.
+					val directory = Paths.get(file.parent)
 					val watcher = FileSystems.getDefault().newWatchService()
 
-					var lines = file.readLines();
-					var lastIndex = lines.size
-
-					// Registers the logDir below with a watch service.
-					val directory = Paths.get(file.parent)
 					directory.register(watcher, ENTRY_CREATE, ENTRY_MODIFY, ENTRY_DELETE)
 
-					// Monitor the logDir at listen for change notification.
+					var lastIndex = getLastLineFromFile(file)
+
 					while (true) {
 
 						val key = watcher.take()
 
 						for (event in key.pollEvents()) {
-
-							val kind = event.kind()
-
-							if (ENTRY_CREATE == kind) {
-								//println("Entry was created on log dir.")
-							} else if (ENTRY_MODIFY == kind) {
-
-								lines = file.readLines()
-
-								val newLines = lines.subList(lastIndex, lines.size);
-
-								lastIndex = lines.size
-
-								val chatMessages = getChatMessages(newLines)
-
-								chatMessages.forEach(reader)
-
-							} else if (ENTRY_DELETE == kind) {
-								//println("Entry was deleted from log dir.")
+							when (event.kind()) {
+								ENTRY_CREATE -> println("Entry was created on log dir.")
+								ENTRY_DELETE -> println("Entry was deleted from log dir.")
+								ENTRY_MODIFY -> {
+									val lines = readLinesFromIndex(file, lastIndex)
+									//update the last index
+									lastIndex += lines.size
+									createChatMessageFromLines(lines).forEach(onChangeCallBack)
+								}
 							}
 						}
+
 						key.reset()
 					}
 				} catch (e: IOException) {
@@ -68,23 +56,14 @@ class JediAcademyLogDataReader(val file: File) {
 			}
 		}
 
-		fun readAllMessages(): List<ChatMessage> {
-
-			val lines = file.readLines()
-
-			return getChatMessages(lines)
-		}
-
-		private fun getChatMessages(lines: List<String>): List<ChatMessage> {
-			return lines.stream()
-					.filter { it.contains(Regex(": say: ")) }
-					.map { toChatMessage(removeColorTags(it)) }
-					.toList()
-		}
+		private fun createChatMessageFromLines(lines: List<String>): List<Message> = lines.stream()
+				.filter { it.contains(Regex(": say: ")) }
+				.map { toChatMessage(removeColorTags(it)) }
+				.toList()
 
 		fun removeColorTags(str: String): String = str.replace(Regex("\\^+[0-9]"), "")
 
-		fun toChatMessage(str: String) : ChatMessage {
+		fun toChatMessage(str: String) : Message {
 
 			val firstSeparator = str.indexOf(":")
 			val secondSeparator = str.indexOf(":", firstSeparator + 1)
@@ -95,21 +74,52 @@ class JediAcademyLogDataReader(val file: File) {
 
 			val message = str.subSequence(fourthSeparator + 2, str.length).replace(Regex("\""), "")
 
-			return ChatMessage(playerName, message)
+			return Message(playerName, message)
 		}
 
-		data class ChatMessage(val playerName: String, val message: String)
+		fun getLastLineFromFile(file: File) : Long {
+			var lastLine = 0L
+			val fileScanner = Scanner(file)
+
+			while (fileScanner.hasNextLine()) {
+				fileScanner.nextLine()
+				lastLine++
+			}
+
+			return lastLine
+		}
+
+		fun readLinesFromIndex(file: File, initalIndex: Long) : List<String> {
+			var lastLine = 0L
+			val fileScanner = Scanner(file)
+			val lines = mutableListOf<String>()
+
+			while (fileScanner.hasNextLine()) {
+				if (lastLine >= initalIndex) {
+					lines.add(fileScanner.nextLine())
+				} else {
+					fileScanner.nextLine()
+				}
+				lastLine++
+			}
+
+			return lines
+		}
+
+		fun readAllMessages(): List<Message> = createChatMessageFromLines(file.readLines())
+
+		data class Message(val playerName: String, val message: String)
 	}
 
 }
 
-//suspend fun main() {
-//	val file = File("server.log")
-//
-//	JediAcademyLogDataReader(file)
-//			.onChat()
-//			.watch { println("${it.playerName}: ${it.message}") }
-//			.await()
-//
-//	//readAllMessages.forEach{println(it)}
-//}
+suspend fun main() {
+	val file = File("/home/admin/server.log")
+
+	JediAcademyLogDataReader(file)
+			.onChat()
+			.watchChanges { println("${it.playerName}: ${it.message}") }
+			.await()
+
+
+}
